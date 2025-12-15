98b24653c59ec6d192958ee62199d33bc260cd0c
VaksHSE <macick@mail.ru>
2025-12-15 15:53:54 +0300

    pre attack

--- Assets/Scripts/Characters/Player/Data/ScriptableObjects/PlayerSO.cs
+++ Assets/Scripts/Characters/Player/Data/ScriptableObjects/PlayerSO.cs
@@ -4,4 +4,17 @@ using UnityEngine;
 public class PlayerSO : ScriptableObject
 {
     [field: SerializeField] public PlayerMovementData MovementData { get; private set; }
+
+    [Header("BasicStatus")]
+    [SerializeField] public string playerName;
+    [SerializeField] public float maxHealth;
+    [SerializeField] public float armour;
+    [SerializeField] public float damage;
+    [SerializeField] public float moveSpeed;
+
+    [Header("RangeConfig")]
+    [SerializeField] public float attackRange;
+
+    [Header("AttackConfig")]
+    [SerializeField] public float attackCooldown;
 }
---
+++ Assets/Scripts/TeEmPe.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public abstract class TeEmPe : MonoBehaviour
+{
+    [SerializeField]
+    protected bool isDestroyOnEnter;
+    [SerializeField]
+    protected float dmg;
+
+    
+}
