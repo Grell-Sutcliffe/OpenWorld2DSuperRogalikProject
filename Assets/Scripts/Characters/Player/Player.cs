b4c863b8967b401f4e63725e3793fd00aef7b6f8
Jenny Akhtiamova <zhanna.axt@bk.ru>
2026-05-17 15:32:36 +0300

    asdfghjk

--- Assets/Scripts/Characters/Player/Player.cs
+++ Assets/Scripts/Characters/Player/Player.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Overlays;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using static ShopPanelScript;
